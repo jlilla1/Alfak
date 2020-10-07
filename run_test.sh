3d86afcce19047809ce8b24ebb8fde498d5b2ceb
PapFe <43848351+PapFe@users.noreply.github.com>
2020-10-07 20:26:59 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -3,9 +3,8 @@
 IFS=$'\n'
 
 
-if test -f out_res.txt do
-	rm out_res.txt
-done
+if test -f out_res.txt; do rm out_res.txt; done
+
 for i in 'cat inputs.txt' do
 	echo "$i" | main >> out_res.txt
 done
