8cd9dee564b969606c288684857a31df587bdda3
PapFe <43848351+PapFe@users.noreply.github.com>
2020-10-07 22:14:19 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -19,7 +19,7 @@ g++  *.cpp -o test
 
 
 
-hile IFS= read -r line \|\| [ [ -n "$line" ] ] ; do
+while IFS= read -r line \|\| [ [ -n "$line" ] ] ; do
    echo "$line" >> out_res.txt
 done < inputs.txt
 
