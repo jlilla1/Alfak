fdb1b4bd052ff5d3a9522a0aeac9df4a25d36c90
PapFe <43848351+PapFe@users.noreply.github.com>
2020-10-07 20:30:26 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -5,6 +5,4 @@ IFS=$'\n'
 
 if test -f out_res.txt do rm out_res.txt done
 
-for i in 'cat inputs.txt' do
-	echo "$i" | main >> out_res.txt
-done
+for i in 'cat inputs.txt' do echo "$i" | main >> out_res.txt done
