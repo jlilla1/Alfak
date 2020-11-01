3048f4bfd2bbb3bbd02f456b69baf2be4643c6d5
PapFe <43848351+PapFe@users.noreply.github.com>
2020-11-01 20:04:35 +0100

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -11,8 +11,8 @@ fi
 cat > out_res.txt
 
 echo `./main units/unit1.json units/unit2.json`
-echo `./main units/unit1.json units/unit3.json` >> "$2"
-echo `./main units/unit2.json units/unit1.json` >> "$2"
-echo `./main units/unit2.json units/unit3.json` >> "$2"
-echo `./main units/unit3.json units/unit1.json` >> "$2"
-echo `./main units/unit3.json units/unit2.json` >> "$2"
+echo `./main units/unit1.json units/unit3.json` 
+echo `./main units/unit2.json units/unit1.json`
+echo `./main units/unit2.json units/unit3.json`
+echo `./main units/unit3.json units/unit1.json`
+echo `./main units/unit3.json units/unit2.json`
