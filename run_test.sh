15c608efd0afca56d424ffe113c636aaa6d0d80f
jlilla1 <janoki.lilla@gmail.com>
2020-12-04 20:25:30 +0100

    teszt proba 2

--- run_test.sh
+++ run_test.sh
@@ -3,9 +3,9 @@
 
 cat > out_res.txt
 
-echo `./main units/unit1.json units/unit2.json` | tee -a out_res.txt
-echo `./main units/unit1.json units/unit3.json` | tee -a out_res.txt
-echo `./main units/unit2.json units/unit1.json` | tee -a out_res.txt
-echo `./main units/unit2.json units/unit3.json` | tee -a out_res.txt
-echo `./main units/unit3.json units/unit1.json` | tee -a out_res.txt
-echo `./main units/unit3.json units/unit2.json` | tee -a out_res.txt
+echo `./k units/unit1.json units/unit2.json` | tee -a out_res.txt
+echo `./k units/unit1.json units/unit3.json` | tee -a out_res.txt
+echo `./k units/unit2.json units/unit1.json` | tee -a out_res.txt
+echo `./k units/unit2.json units/unit3.json` | tee -a out_res.txt
+echo `./k units/unit3.json units/unit1.json` | tee -a out_res.txt
+echo `./k units/unit3.json units/unit2.json` | tee -a out_res.txt
