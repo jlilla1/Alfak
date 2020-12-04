0b525d83be41356f02975709c177ff05569b1efa
jlilla1 <janoki.lilla@gmail.com>
2020-12-04 20:34:19 +0100

    teszt proba 4

--- game.cpp
+++ game.cpp
@@ -35,17 +35,17 @@ void Game::Fight() {
 	/// i counts the rounds, in the firs Round A attacks than B punches and we start the timers. From the second round the figth is controlled by the timers.
 	while (A.getAkthp() > 0 && B.getAkthp() > 0)
 	{
-		std::cout << i<<". kor"<<"\n\n";
+		/*std::cout << i<<". kor"<<"\n\n";
 		std::cout << A.Getname() << "  " << A.getAkthp() << " " << A.getLvl() << "\n\n";
-		std::cout << B.Getname() << " " << B.getAkthp() << " " << B.getLvl() << "\n\n";
+		std::cout << B.Getname() << " " << B.getAkthp() << " " << B.getLvl() << "\n\n";*/
 		
 
 		if (i == 1) {
 			A.getAttackedby(B);
-			std::cout << A.Getname() << "  " << A.getAkthp() << " " << A.getLvl() << "\n\n";
+			//std::cout << A.Getname() << "  " << A.getAkthp() << " " << A.getLvl() << "\n\n";
 			if (A.getAkthp() != 0) {
 				B.getAttackedby(A);
-				std::cout << B.Getname() << " " << B.getAkthp() << " " << B.getLvl() << "\n\n";
+				//std::cout << B.Getname() << " " << B.getAkthp() << " " << B.getLvl() << "\n\n";
 				lastAttack = 1;
 				timerA = A.GetAttackCooldown();
 				timerB = B.GetAttackCooldown();
@@ -59,7 +59,7 @@ void Game::Fight() {
 		else if (timerA < timerB) {
 
 			B.getAttackedby(A);
-			std::cout << B.Getname() << " " << B.getAkthp() << " " << B.getLvl() << "\n\n";
+			//std::cout << B.Getname() << " " << B.getAkthp() << " " << B.getLvl() << "\n\n";
 			lastAttack = 0;
 			timerA = timerA + A.GetAttackCooldown();
 			i++;
@@ -68,7 +68,7 @@ void Game::Fight() {
 		else if (timerB < timerA) {
 
 			A.getAttackedby(B);
-			std::cout << A.Getname() << "  " << A.getAkthp() << " " << A.getLvl() << "\n\n";
+			//std::cout << A.Getname() << "  " << A.getAkthp() << " " << A.getLvl() << "\n\n";
 			lastAttack = 1;
 			timerB = timerB + B.GetAttackCooldown();
 			i++;
@@ -77,14 +77,14 @@ void Game::Fight() {
 		else {
 			if (lastAttack == 0) {
 				B.getAttackedby(A);
-				std::cout << B.Getname() << " " << B.getAkthp() << " " << B.getLvl() << "\n\n";
+				//std::cout << B.Getname() << " " << B.getAkthp() << " " << B.getLvl() << "\n\n";
 				timerA = timerA + A.GetAttackCooldown();
 				lastAttack = 1;
 				i++;
 			}
 			else {
 				A.getAttackedby(B);
-				std::cout << A.Getname() << "  " << A.getAkthp() << " " << A.getLvl() << "\n\n";
+				//std::cout << A.Getname() << "  " << A.getAkthp() << " " << A.getLvl() << "\n\n";
 				timerB = timerB + B.GetAttackCooldown();
 				lastAttack = 0;
 				i++;
@@ -97,10 +97,10 @@ void Game::Fight() {
 
 	/// This part figures out form remaining HP that who is the winner and who is the loser, than prints out the results.
 	if (A.getAkthp() == 0) {
-		std::cout << B.Getname() << " WINS. Remaining Hp: " << B.getAkthp() << " "<<B.getLvl()<<"\n\n";
+		std::cout << B.Getname() << " WINS. Remaining Hp: " << B.getAkthp() << "\n\n";
 	}
 	else {
-		std::cout << A.Getname() << " WINS. Remaining Hp: " << A.getAkthp() << " " << A.getLvl() << "\n\n";
+		std::cout << A.Getname() << " WINS. Remaining Hp: " << A.getAkthp() << "\n\n";
 	}
 }
 
