fa68a44050343e48d24bc2810ac0490e5bb0ca86
KViktor58 <kunviktor58@gmail.com>
2020-12-06 20:42:47 +0100

    recaftoTest3

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat > out_res.txt
-echo `./k scenario/scenario1.json` | tee -a out_res.txt
-echo `./k scenario/scenario2.json` | tee -a out_res.txt
+echo `./k scenario1.json` | tee -a out_res.txt
+echo `./k scenario2.json` | tee -a out_res.txt
