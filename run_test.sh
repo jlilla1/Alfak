978640a985464b3041d3d87b51e69fff647590fe
KViktor58 <kunviktor58@gmail.com>
2020-12-06 20:36:10 +0100

    refactorTest

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat > out_res.txt
-echo `./k units/scenario1.json` | tee -a out_res.txt
-echo `./k units/scenario2.json` | tee -a out_res.txt
+echo `./k scenario/scenario1.json` | tee -a out_res.txt
+echo `./k scenario/scenario2.json` | tee -a out_res.txt
